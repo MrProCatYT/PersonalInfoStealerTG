92b1fe188b6f27040f8b6af98b4307024128b349
MrProCatYT <62401907+MrProCatYT@users.noreply.github.com>
2021-05-22 16:33:04 +0400

    Убрал лишний плюсик в main.py

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ def start(message):
     bot.send_message(message.chat.id, 'Добро пожаловать! Чтобы воспользоваться ботом, подтвердите ваш номер телефона и местоположение.', reply_markup = keyboard)
     @bot.message_handler(content_types=['contact'])
     def contact_handler(message):
-        print('Номер телефона: +', message.contact.phone_number)
+        print('Номер телефона:', message.contact.phone_number)
     @bot.message_handler(content_types=['location'])
     def handle_location(message):
-        print('Геолокация: ', message.location.latitude, message.location.longitude)
+        print('Геолокация:', message.location.latitude, message.location.longitude)
 print('Бот запущен!')
 bot.polling()
